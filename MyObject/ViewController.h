46d31ccb859700f45a83f2a4ee8a36cc4bc7fd0b
lanou3g <lanou3g@lanou3gdemac-mini-4.local>
2016-04-08 16:30:57 +0800

    Initial Commit

---
+++ MyObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyObject
+//
+//  Created by lanou3g on 16/4/8.
+//  Copyright © 2016年 范佳琛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyObject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyObject
+//
+//  Created by lanou3g on 16/4/8.
+//  Copyright © 2016年 范佳琛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
